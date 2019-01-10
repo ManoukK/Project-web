99089a516c434d3e01cdfa345cdfda7845b82456
ManoukK <manouk.kappe21@gmail.com>
2019-01-10 18:48:08 +0100

    Deel van animatie 1 af

---
+++ overig/script.js
@@ -0,0 +1,22 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+
+console.log('hoi');
+
+//var button = document.querySelector('button');
+
+//button.addEventListener('click', function() {
+//  document.body.classList.toggle('laad');
+//});
+
+
+
+var testtest = document.querySelector('.normaal');
+
+testtest.addEventListener ('click', function() {
+    testtest.classList.toggle('laad');
+});
